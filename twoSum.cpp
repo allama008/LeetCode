3cac2e06866d265dbde55d93f4e3ca46730208d7
Allama Hossain <a2hossain2000@yahoo.com>
2022-01-11 09:51:54 -0600

    Multiple Questions and Improvement to twoSum using Hash

--- twoSum.cpp
+++ twoSum.cpp
@@ -6,12 +6,33 @@ https://leetcode.com/problems/two-sum/
 #include<iostream>
 #include<stdio.h>
 #include<vector>
+#include<unordered_map>
 using namespace std;
 
 class Solution 
 {
     public:
         vector<int> twoSum(vector<int>& nums, int target) 
+        {
+            vector<int> vec;
+            unordered_map<int, int> umap;
+            int diff;
+            for(int i = 0; i < nums.size(); i++)
+            {
+                diff = target - nums[i];
+                if(umap.find(diff) != umap.end())
+                {
+                    vec.push_back(i);
+                    vec.push_back(umap.find(diff)->second);
+                    return vec;
+                }
+                umap[nums[i]] = i;
+            }
+            return vec;
+        }
+        
+        //Below solution is fast, but not that fast
+        /*vector<int> twoSum(vector<int>& nums, int target) 
         {
             vector<int> vec;
             for(auto i = nums.begin(); i != nums.end(); i++)
@@ -22,16 +43,29 @@ class Solution
                         vec.insert(it, int(i - nums.begin()));
                     }
             return vec;           
-            
-        }
+        }*/
+
+        //Below solution is slower. Try using iterators.
+        /*vector<int> twoSum(vector<int>& nums, int target) 
+        {
+            vector<int> vec;
+            for(auto i = 0; i < nums.size(); i++)
+                for(auto j = i + 1; j < nums.size(); j++)
+                    if(nums[i] + nums[j] == target)
+                    {
+                        vec.push_back(i);
+                        vec.push_back(j);
+                    }
+            return vec;             
+        }*/
 };
 
 int main()
 {
     Solution solobj;
     //solobj.printHello();
-    vector<int> Sol = {2, 7, 11, 15};
-    Sol = solobj.twoSum(Sol, 9);
+    vector<int> Sol = {3, 2, 4};
+    Sol = solobj.twoSum(Sol, 6);
     for(auto i = Sol.begin(); i != Sol.end(); i++)
         cout<<*i<<" ";
     return 0;
